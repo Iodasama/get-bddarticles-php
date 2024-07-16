e2664112a071a17df6b3901358ce56c323515477
Jerome Trezeguet <jerome.trezeguet@lapiscine.pro>
2024-07-16 15:03:37 +0200

    refactoIndexController

--- controller/indexController.php
+++ controller/indexController.php
@@ -1,21 +1,46 @@
 <?php
-require_once('../config/config.php');
+
+//on cree une classe avec une methode index
+class IndexController { 
+//on cree la methode
+    public function index() { 
+
+    require_once('../config/config.php');
 
 
-$dbConnection= new Dbconnection();
-$pdo = $dbConnection->connect();
-//$products = [];
-// Préparer la requête GET
+    $dbConnection= new Dbconnection();
+    $pdo = $dbConnection->connect();
+    
+    // Préparer la requête GET
+    
+    $stmt = $pdo->query("SELECT * FROM articles");
+    
+    
+    
+    
+    // Exécuter la requête on recupere
+    $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    require_once('../templates/page/indexView.php'); // on require la pageView apres avoir recuperer les données de la BDD pour afficher (on n'appelle plus le controller depuis la pageindexView mais elle est appelée ici dans le controller)
 
-$stmt = $pdo->query("SELECT * FROM articles");
+
+}
+
+}
+
+//on instancie la classe avec new 
+// on appelle la methode
+
+$IndexController= new IndexController();
+$IndexController->index();
 
 
 
 
-// Exécuter la requête on recupere
-$articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-require_once('../templates/page/indexView.php');
+
+
+
 
 
 
