8a6cc056d21036b59e2df9ca774f69d32e9f9edd
Jerome Trezeguet <jerome.trezeguet@lapiscine.pro>
2024-07-17 14:13:09 +0200

    commit_MVC

--- controller/addArticleController.php
+++ controller/addArticleController.php
@@ -1,5 +1,7 @@
 <?php
 require_once("../config/config.php");
+require_once("../model/ArticleRepository.php");
+
 // j instancie ma classe
 class AddArticleController {
 
@@ -10,43 +12,45 @@ class AddArticleController {
             // var_dump( value: 'ajouter article');
 
 
-            // je me connecte a la Bdd
-            $dbConnection = new DbConnection();
-            $pdo = $dbConnection->connect();
+            // je me connecte a la Bdd -> Model
+            // $dbConnection = new DbConnection();
+            // $pdo = $dbConnection->connect();
 
-
-
-            // Préparer la requête d'insertion
-            // on prepare ce que l on va executer plus loin, on prepare donc ici la requete d'insertion avant d'executer 
-
-
-            $sql = "INSERT INTO articles (title, content, created_at) VALUES (:title, :content, :created_at)"; //avec VALUES on evite quelqu un rentre du sql
-            $stmt = $pdo->prepare($sql);
-
-            // Définir les paramètres et exécuter
-            // on definit les parametres, on ne rajoute pas date a bindParam qui se cree par defaut 
-            //je prepare les valeurs pour l'INSERT 
+            //je prepare les valeurs pour l'INSERT -> Controller
 
             $title ="Mon super article";
             $content="实在太棒了";
             $date="2024-07-17"; // penser a rentrer format "Y-M-D" pour la date on pourra le modifier dans format par la suite
 
 
+            $articleRepository = new ArticleRepository;
+            $articleRepository->insert($title,$content,$date); 
+            
+            // Préparer la requête d'insertion
+            // on prepare ce que l on va executer plus loin, on prepare donc ici la requete d'insertion avant d'executer 
+            // -> Model
 
-        $stmt->bindParam(':title', $title);
-        $stmt->bindParam(':content', $content);
-        $stmt->bindParam(':created_at', $date);
+            // $sql = "INSERT INTO articles (title, content, created_at) VALUES (:title, :content, :created_at)"; //avec VALUES on evite quelqu un rentre du sql
+            // $stmt = $pdo->prepare($sql);
+
+            // // Définir les paramètres et exécuter
+            // // on definit les parametres
+
+            // $stmt->bindParam(':title', $title);
+            // $stmt->bindParam(':content', $content);
+            // $stmt->bindParam(':created_at', $date);
 
 
 
-        // Exécuter la requête
+    //     // Exécuter la requête -> View
 
-        if ($stmt->execute()) {
-            echo "Nouvel article ajouté avec succès";
-        } else {
-            echo "Erreur lors de l'ajout du produit";
-        }
-    } 
+    //     if ($stmt->execute()) {
+    //         echo "Nouvel article ajouté avec succès";
+    //     } else {
+    //         echo "Erreur lors de l'ajout du produit";
+    //     }
+    // } 
+}
 }
 //on instancie la classe avec new 
 // on appelle la methode
--- model/ArticleRepository.php
+++ model/ArticleRepository.php
@@ -22,8 +22,37 @@
 
                 return $articles; // ne pas oublier de return pour la recuperation
     }
-}   
+
+    public function insert($title,$content,$date) { 
+
+            // je me connecte a la Bdd
+            $dbConnection= new Dbconnection();
+            $pdo = $dbConnection->connect();
+            // Préparer la requête d'insertion
+            // on prepare ce que l on va executer plus loin, on prepare donc ici la requete d'insertion avant d'executer 
+
+            $sql = "INSERT INTO articles (title, content, created_at) VALUES (:title, :content, :created_at)"; //avec VALUES on evite quelqu un rentre du sql
+            $stmt = $pdo->prepare($sql);
+
+            // Définir les paramètres et exécuter
+            // on definit les parametres
+
+            $stmt->bindParam(':title', $title);
+            $stmt->bindParam(':content', $content);
+            $stmt->bindParam(':created_at', $date);
         
+            // Exécuter la requête pour affichage ici dans modele mais plus tard -> View 
+
+            if ($stmt->execute()) {
+                echo "Nouvel article ajouté avec succès";
+            } else {
+                echo "Erreur lors de l'ajout du produit";
+            }
+            } 
+
+
+    }
+      
         
         
     
